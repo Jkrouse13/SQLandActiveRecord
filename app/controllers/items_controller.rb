8a92985391377fb199e80f6faca9713a1e104415
jkrouse13 <jon.krouse@gmail.com>
2016-10-26 15:11:28 -0400

    Added create new item feature edit item feature in progress.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,37 @@ class ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new
+    @item.title = params[:item][:title]
+    @item.description = params[:item][:description]
+    @item.category = params[:item][:category]
+    @item.price = params[:item][:price]
+    @item.save
+    redirect_to root_path
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    render :new
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.title = params[:item][:title]
+    @item.description = params[:item][:description]
+    @item.category = params[:item][:category]
+    @item.price = params[:item][:price]
+    @item.save
+    redirect_to item_path(id: @item.id)
+  end
+
 end
